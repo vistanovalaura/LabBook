4040b4667b360c283ddb525c8235e6800530d9ba
lauravistanova <lauravistanova@158.197.238.74>
2018-11-13 10:22:03 +0100

    alertBox pre neuspesne prihalsenie

---
+++ LabBook/src/main/java/test/Tester.java
@@ -0,0 +1,18 @@
+package test;
+
+import java.util.List;
+
+import sk.upjs.paz1c.entities.User;
+import sk.upjs.paz1c.persistent.DAOfactory;
+import sk.upjs.paz1c.persistent.UserDAO;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+UserDAO userDAO = DAOfactory.INSTANCE.getUserDAO();
+List<User> users = userDAO.getAll();
+System.out.println(users.size());
+	}
+
+}
