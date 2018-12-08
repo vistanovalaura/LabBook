c1a1ad8ee065906fd03a9933ad24ccc539b6aa28
Oliver <Oliver@ideaY510P-NB>
2018-12-08 15:22:42 +0100

    UserIdentificationManagerTest

--- LabBook/src/test/java/sk/upjs/paz1c/business/UserIdentificationManagerTest.java
+++ LabBook/src/test/java/sk/upjs/paz1c/business/UserIdentificationManagerTest.java
@@ -1,13 +1,67 @@
 package sk.upjs.paz1c.business;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
+import sk.upjs.paz1c.entities.Admin;
 import sk.upjs.paz1c.entities.User;
+import sk.upjs.paz1c.persistent.AdminDAO;
 import sk.upjs.paz1c.persistent.DAOfactory;
 import sk.upjs.paz1c.persistent.UserDAO;
 
 public class UserIdentificationManagerTest {
-	
+
+	@Test
+	void setUserTest() {
+		User testUser = new User();
+		testUser.setName("tester");
+		testUser.setPassword("1234");
+		testUser.setEmail("tester.testovaci@test.com");
+		UserDAO userDAO = DAOfactory.INSTANCE.getUserDAO();
+		userDAO.addUser(testUser);
+
+		int output = UserIdentificationManager.setUser("tester", "1234");
+		assertEquals(UserIdentificationManager.getTypeOfUser(), 1);
+		assertEquals(UserIdentificationManager.getId(), testUser.getUserID());
+		assertEquals(output, 1);
+
+		userDAO.deleteUser(testUser);
+
+		Admin testAdmin = new Admin();
+		testAdmin.setName("tester");
+		testAdmin.setPassword("1234");
+		AdminDAO adminDAO = DAOfactory.INSTANCE.getAdminDAO();
+		adminDAO.addAdmin(testAdmin);
+
+		output = UserIdentificationManager.setUser("tester", "1234");
+		assertEquals(UserIdentificationManager.getTypeOfUser(), 2);
+		assertEquals(UserIdentificationManager.getId(), testAdmin.getAdminID());
+		assertEquals(output, 2);
+
+		adminDAO.deleteAdmin(testAdmin);
+	}
+
+	@Test
+	void logOutTest() {
+		User testUser = new User();
+		testUser.setName("tester");
+		testUser.setPassword("1234");
+		testUser.setEmail("tester.testovaci@test.com");
+		UserDAO userDAO = DAOfactory.INSTANCE.getUserDAO();
+		userDAO.addUser(testUser);
+
+		UserIdentificationManager.setUser("tester", "1234");
+		assertEquals(UserIdentificationManager.getTypeOfUser(), 1);
+		assertEquals(UserIdentificationManager.getId(), testUser.getUserID());
+
+		UserIdentificationManager.logOut();
+		assertEquals(UserIdentificationManager.getTypeOfUser(), 0);
+		assertEquals(UserIdentificationManager.getId(), null);
+
+		userDAO.deleteUser(testUser);
+	}
+
 	@Test
 	void getUserTest() {
 		User testUser = new User();
@@ -16,8 +70,25 @@ public class UserIdentificationManagerTest {
 		testUser.setEmail("tester.testovaci@test.com");
 		UserDAO userDAO = DAOfactory.INSTANCE.getUserDAO();
 		userDAO.addUser(testUser);
+
+		UserIdentificationManager.setUser("tester", "1234");
+		assertEquals(UserIdentificationManager.getUser().getUserID(), testUser.getUserID());
+
+		userDAO.deleteUser(testUser);
+	}
+
+	@Test
+	void getAdminTest() {
+		Admin testAdmin = new Admin();
+		testAdmin.setName("tester");
+		testAdmin.setPassword("1234");
+		AdminDAO adminDAO = DAOfactory.INSTANCE.getAdminDAO();
+		adminDAO.addAdmin(testAdmin);
 		
+		UserIdentificationManager.setUser("tester", "1234");
+		assertEquals(UserIdentificationManager.getAdmin().getAdminID(), testAdmin.getAdminID());
 		
+		adminDAO.deleteAdmin(testAdmin);
 	}
 
 }
