e426e8544433d5564073bea96226900f4651a04e
Oliver <Oliver@ideaY510P-NB>
2018-12-16 12:25:11 +0100

    opravy
    opraveny kurzor, vymazany zakomentovany kod v app a FIXME v
    newItemController

--- LabBook/src/main/java/sk/upjs/paz1c/gui/App.java
+++ LabBook/src/main/java/sk/upjs/paz1c/gui/App.java
@@ -1,18 +1,10 @@
 package sk.upjs.paz1c.gui;
 
-import java.time.LocalDate;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import sk.upjs.paz1c.entities.Admin;
-import sk.upjs.paz1c.entities.Project;
-import sk.upjs.paz1c.entities.User;
-import sk.upjs.paz1c.persistent.DAOfactory;
-import sk.upjs.paz1c.persistent.ProjectDAO;
-import sk.upjs.paz1c.persistent.UserDAO;
 
 public class App extends Application {
 
@@ -21,6 +13,7 @@ public class App extends Application {
 		FrontPageController mainController = new FrontPageController();
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("frontPage.fxml"));
 		fxmlLoader.setController(mainController);
+		// môže hodiť IOException
 		Parent rootPane = fxmlLoader.load();
 
 		Scene scene = new Scene(rootPane);
@@ -31,41 +24,6 @@ public class App extends Application {
 
 	public static void main(String[] args) {
 		launch(args);
-		
-		
-		// Project project = DAOfactory.INSTANCE.getProjectDAO().getByName("urobenie
-		// save");
-		// project.setName("urobenie_save");
-		// DAOfactory.INSTANCE.getProjectDAO().saveProject(project);
-		// System.out.println(DAOfactory.INSTANCE.getProjectDAO().getByName("urobenie_save"));
-
-		// User oliver = new User();
-		// oliver.setName("Laura");
-		// oliver.setPassword("2357");
-		// oliver.setEmail("laura.vistanova@gmail.com");
-		// UserDAO userDAO = DAOfactory.INSTANCE.getUserDAO();
-		// userDAO.addUser(oliver);
-		// System.out.println(userDAO.getAll());
-		//
-		//
-		// Project project = new Project();
-		// project.setName("spravenie save");
-		// project.setActive(true);
-		// project.setDateFrom(LocalDate.now());
-		// project.setDateUntil(LocalDate.now());
-		// project.setEachItemAvailable(true);
-		// ProjectDAO projectDAO = DAOfactory.INSTANCE.getProjectDAO();
-		// projectDAO.saveProject(project);
-		// project.setName("urobenie save");
-		// projectDAO.saveProject(project);
-		//
-		// project = new Project();
-		// project.setName("nezaspat");
-		// project.setActive(true);
-		// project.setDateFrom(LocalDate.now());
-		// project.setEachItemAvailable(false);
-		// projectDAO.addProject(project);
-		// System.out.println(projectDAO.getAll());
 
 	}
 
